e90a2dc51c923da8c8e52e113203e71836d13720
Abraham Kuri <kurenn@icalialabs.com>
2015-07-02 13:56:19 -0500

    Requires all files

--- lib/hsnews_rb.rb
+++ lib/hsnews_rb.rb
@@ -1,6 +1,10 @@
 require "hsnews_rb/version"
 require "httparty"
+require "hsnews_rb/base"
+require "hsnews_rb/session"
 require "hsnews_rb/link"
+require "hsnews_rb/user"
+require "hsnews_rb/comment"
 
 module HsnewsRb
   # Your code goes here...
