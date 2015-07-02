78a4f74a06a4cba00f10861c95a508f71d4c5e4a
Abraham Kuri <kurenn@icalialabs.com>
2015-07-02 14:05:04 -0500

    Updates gem endpoint

--- lib/hsnews_rb/base.rb
+++ lib/hsnews_rb/base.rb
@@ -1,7 +1,7 @@
 module HsnewsRb
   class Base < OpenStruct
     include HTTParty
-    base_uri "http://localhost:3000/api"
+    base_uri "https://staging-hsnews.herokuapp.com/api"
     headers "Accept" => "application/vnd.hsnews.com+json; version=1"
   end
 end
